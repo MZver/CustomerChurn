3e151f4c243811956abac9813f5ba98c303c2255
Matija Zver <matijazver7@gmail.com>
2022-02-11 12:01:28 +0100

    Second

--- tests/testthat/Highes_Lowest_Test.R
+++ tests/testthat/Highes_Lowest_Test.R
@@ -1,5 +1,7 @@
 context("Testing")
 
 test_that("if the customer with the highes churn_prob actually has it higher", {
-  expect_gt(highest_churn(merge_file), lowest_churn(merge_file))
+  expect_gt(churn_calculation(merge_file, highest_churn), churn_calculation(merge_file, lowest_churn))
 })
+
+
